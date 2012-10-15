a810e01f0911f3bc5b6ca97f3cee117c154ffe0d
Dana Benson <danabenson@gmail.com>
2012-10-14 17:00:39 -0700

    initial commit

---
+++ ZeroDay.API/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using ZeroDay.API.App_Start;
+
+namespace ZeroDay.API
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+        }
+    }
+}
